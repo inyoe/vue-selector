52eab64500529b0af2d60c487575b096d86215ce
Peter Pan <3332474@qq.com>
2017-05-11 15:17:12 +0800

    Update selector.vue

--- selector/selector.vue
+++ selector/selector.vue
@@ -41,11 +41,11 @@ export default {
 
 
 <style scoped>
-.selector{position:relative; z-index:2; box-sizing:border-box; width:100%; height:100%; padding:10px; border-radius:4px; border:1px solid #ddd; line-height:22px;}
+.selector{position:relative; box-sizing:border-box; width:100%; height:100%; padding:10px; border-radius:4px; border:1px solid #ddd; line-height:22px;}
 .selector:before{content:''; position:absolute; top:50%; right:15px; width:6px; height:6px; border-right:1px solid #999; border-bottom:1px solid #999; -webkit-transform:translateY(-50%) rotate(45deg); -moz-transform:translateY(-5px) rotate(45deg); transform:translateY(-5px) rotate(45deg); -webkit-transition:.2s; transition:.2s;}
 .selector.act:before{-webkit-transform:translateY(-2px) rotate(-135deg); transform:translateY(-2px) rotate(-135deg);}
 .selector h2{line-height:22px;}
-.selector-option{position:absolute; top:100%; left:0; width:100%; max-height:276px; margin-top:3px; border-radius:4px; box-shadow:0 0 0 1px #ddd; background:#fff; overflow:auto; -webkit-transition:.2s; transition:.2s;}
+.selector-option{position:absolute; top:100%; left:0; z-index:2; width:100%; max-height:276px; margin-top:3px; border-radius:4px; box-shadow:0 0 0 1px #ddd; background:#fff; overflow:auto; -webkit-transition:.2s; transition:.2s;}
 .selector-option li{height:52px; padding:0 15px; border-bottom:1px solid #ddd; line-height:52px; -webkit-user-select:none;}
 .selector-option li:last-of-type{border-bottom:0;}
 .selector-option li:active{background:#eee;}
